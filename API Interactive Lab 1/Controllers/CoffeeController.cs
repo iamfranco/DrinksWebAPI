030d12ad8a20c2270204303b3fd775985a24c85a
iamfranco <kinfaifrancochan@gmail.com>
2022-06-21 09:36:57 +0100

    add CoffeeController with endpoint /lover, with unit test

---
+++ API Interactive Lab 1/Controllers/CoffeeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Interactive_Lab_1.Controllers;
+
+[ApiController]
+[Route("coffee")]
+public class CoffeeController : ControllerBase
+{
+    [HttpGet("lover")]
+    public string GetLover()
+    {
+        return "I like coffee!";
+    }
+}
---
+++ API Interactive Lab 1/Tests/Controllers/CoffeeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using API_Interactive_Lab_1.Controllers;
+using NUnit.Framework;
+
+namespace API_Interactive_Lab_1.Tests.Controllers;
+
+public class CoffeeControllerTests
+{
+    [Test]
+    public void TestGetCoffeeLover()
+    {
+        string expectedResult = "I like coffee!";
+
+        var controller = new CoffeeController();
+
+        string result = controller.GetLover();
+
+        Assert.AreEqual(expectedResult, result);
+    }
+}
